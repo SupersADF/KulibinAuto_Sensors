925dbc3681cda25851307a72111139c28314e404
Sergey Mishin <supers.ya@yandex.ru>
2019-12-10 22:45:00 +0300

    upl: добавил bluetooth

--- src/KulibinAuto_Sensors.cpp
+++ src/KulibinAuto_Sensors.cpp
@@ -6,13 +6,12 @@ KulibinAuto_Sensors::KulibinAuto_Sensors(uint8_t trig, uint8_t echo, uint8_t ser
 	SERVO_PIN = servo;
 	pinMode(TRIG_PIN, OUTPUT);
 	pinMode(ECHO_PIN, INPUT);
-	// myServo.attach(SERVO_PIN);
 }
 KulibinAuto_Sensors::KulibinAuto_Sensors() {
 	pinMode(TRIG_PIN, OUTPUT);
 	pinMode(ECHO_PIN, INPUT);
+	bluetooth_serial.begin(9600);
 }
-
 float KulibinAuto_Sensors::getUltraDistance(uint8_t dimension) {
 	if (dimension == ULTRA_CM) {
 		float dist = getTiming() / 58.0;
@@ -29,7 +28,7 @@ float KulibinAuto_Sensors::getUltraDistance(uint8_t dimension) {
 		if (dist == 0) 
 			dist = 0.16;
 		return dist;
-	}
+	} else return 0;
 }
 void KulibinAuto_Sensors::setServoAngle(int angle) {
 	myServo.attach(SERVO_PIN);
@@ -43,17 +42,17 @@ uint8_t KulibinAuto_Sensors::getReflect(uint8_t num) {
 		return !digitalRead(REFLECT_2_PIN);
 	} else if (num == 3) {
 		return !digitalRead(REFLECT_3_PIN);
-	}
+	} else return 0;
 }
 void KulibinAuto_Sensors::setUpColorSensor(uint8_t IntegrationTime = TCS34725_INTEGRATIONTIME_2_4MS, uint8_t Gain_t = TCS34725_GAIN_1X) {
-	color_sensor = Adafruit_TCS34725(IntegrationTime, Gain_t);
+	color_sensor = Adafruit_TCS34725((tcs34725IntegrationTime_t)IntegrationTime, (tcs34725Gain_t)Gain_t);
 	// color_sensor = color_sensor_t;
 	if (color_sensor.begin()) {
     	Serial.println("Found sensor");
   	}
 }
 void KulibinAuto_Sensors::getColorRawData(uint16_t *r, uint16_t *g, uint16_t *b, uint16_t *c) {
-	uint16_t r_, g_, b_, c_, colorTemp, lux;
+	uint16_t r_, g_, b_, c_;//, colorTemp, lux;
 	color_sensor.getRawData(&r_, &g_, &b_, &c_);
 	*r = r_;
 	*g = g_;
@@ -76,6 +75,33 @@ void KulibinAuto_Sensors::setColorGain(tcs34725Gain_t gain) {
 void KulibinAuto_Sensors::setColorIntegrationTime(tcs34725IntegrationTime_t it) {
 	color_sensor.setIntegrationTime(it);
 }
+void KulibinAuto_Sensors::getBluetoothMessage(uint8_t *return_array, uint8_t print_serial = 0) {
+	if (bluetooth_serial.available() > 0) {
+		uint8_t start_data = bluetooth_serial.read(); 
+		if (start_data == 243 || start_data == 241 || start_data == 242) { 
+			uint8_t data[3];
+			for (int i = 0; i < 3; i++) {
+				delay(1);
+				data[i] = bluetooth_serial.read();
+			}
+			if (data[2] != 243 || data[2] != 242 || data[2] != 241) {
+        		command.F_ = start_data; //direction
+        		command.speed = data[0]; //speed
+        		command.turn = data[1];  //turn
+        		command.command_machine = data[2]; //lights and buttons
+				if (print_serial != 0) {
+					Serial.println(String("dir - ") + command.F_ + String("  speed - ") + command.speed + 
+								String("  turn - ") + command.turn + String("  commands - ") + command.command_machine);
+				}
+				return_array[0] = command.F_;
+				return_array[1] = command.speed;
+				return_array[2] = command.turn;
+				return_array[3] = command.command_machine;
+      		}
+		}		
+	}
+}
+
 
 //private 
 
--- src/KulibinAuto_Sensors.h
+++ src/KulibinAuto_Sensors.h
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Servo.h>
 #include <tcs34725.h>
+#include <SoftwareSerial.h>
 
 #define ULTRA_CM 		1
 #define ULTRA_MM		2
@@ -10,12 +11,13 @@ class KulibinAuto_Sensors {
 
 Servo myServo;
 Adafruit_TCS34725 color_sensor;
+SoftwareSerial bluetooth_serial = SoftwareSerial(10, A3); //RX, TX
 
 public:
 	KulibinAuto_Sensors(uint8_t trig, uint8_t echo, uint8_t servo, uint8_t reflect_1, uint8_t reflect_2, uint8_t reflect_3);
 	KulibinAuto_Sensors();
 
-	void setUpColorSensor(uint8_t IntegrationTime = TCS34725_INTEGRATIONTIME_2_4MS, uint8_t Gain_t = TCS34725_GAIN_1X);
+	void setUpColorSensor(uint8_t IntegrationTime, uint8_t Gain_t);
 	void getColorRawData(uint16_t *r, uint16_t *g, uint16_t *b, uint16_t *c);
 	void getColorRGB(float *r, float *g, float *b);
 	uint16_t calculateColorLux(uint16_t r, uint16_t g, uint16_t b);
@@ -27,13 +29,22 @@ public:
 
 	uint8_t getReflect(uint8_t num);
 
+	void getBluetoothMessage(uint8_t *return_array, uint8_t print_serial);
 private:
 	uint32_t getTiming();
+
+	struct command_t {
+  		uint8_t F_ =              0x00;
+  		uint8_t speed =           0x00;
+  		uint8_t turn =            0x00;
+  		uint8_t command_machine = 0x00;
+	} command;
 
 private:
 	uint8_t TRIG_PIN = 13;
 	uint8_t ECHO_PIN = 9;
 	uint8_t SERVO_PIN = 11;
+	uint8_t BLUETOOTH_PIN = 10;
 
 	uint8_t REFLECT_1_PIN = A0;
 	uint8_t REFLECT_2_PIN = A1;
